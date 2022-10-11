ecf9b7e597aa8bd54030551193240f4501d1b18c
zong-xun <65030074+zong-xun@users.noreply.github.com>
2022-10-11 15:41:32 +0800

    第一頁
    第一頁RWD

---
+++ js/all.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+        $(".menu").toggleClass('menu_open');
+	});
+});
