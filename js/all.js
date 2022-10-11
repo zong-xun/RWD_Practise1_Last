9d04418f25f21eee5678613cdf1cb4c7d08e0cb8
zong-xun <65030074+zong-xun@users.noreply.github.com>
2022-10-11 16:09:23 +0800

    men樣式調整

--- js/all.js
+++ js/all.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
 	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
 		$(this).toggleClass('open');
         $(".menu").toggleClass('menu_open');
+		$(".menu ul").toggleClass('menu_ul_open');
 	});
 });
