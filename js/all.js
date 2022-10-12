8dc89e58b39c2a8af6666b316a1da2f0a8ea7e63
zong-xun <65030074+zong-xun@users.noreply.github.com>
2022-10-12 16:48:27 +0800

    menu_page

--- js/all.js
+++ js/all.js
@@ -2,6 +2,5 @@ $(document).ready(function(){
 	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
 		$(this).toggleClass('open');
         $(".menu").toggleClass('menu_open');
-		$(".menu ul").toggleClass('menu_ul_open');
 	});
 });
